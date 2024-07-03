79641d97689d2fb4e7cd9c18a39594b520f3d0c9
nagosu <nozick1021@gmail.com>
2024-07-03 23:13:30 +0900

    menu03 더미 API 연동 완료

--- static/chatbot-admin/js/menu03.js
+++ static/chatbot-admin/js/menu03.js
@@ -1,8 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleList = document.querySelectorAll(
-    ".language-setting__toggle-switch"
-  );
+const toggleEnglish = document.querySelector(
+  ".language-setting__toggle-switch.english"
+); // 토글 스위치 (영어)
+const toggleChinese = document.querySelector(
+  ".language-setting__toggle-switch.chinese"
+); // 토글 스위치 (중국어)
+const toggleInputEnglish = document.getElementById("toggle-english"); // 토글 스위치 (영어) input
+const toggleInputChinese = document.getElementById("toggle-chinese"); // 토글 스위치 (중국어) input
+const toggleList = document.querySelectorAll(
+  ".language-setting__toggle-switch"
+); // 전체 토글 스위치 (영어/중국어)
 
+let toggleStatusEnglish = false; // 토글 스위치 (영어) 상태
+let toggleStatusChinese = false; // 토글 스위치 (중국어) 상태
+
+// 언어 설정 저장 API 호출
+async function saveLanguageSetting(language) {
+  // const url = "https://example.com"; // 실제 API URL로 변경 필요
+  let data = {};
+
+  if (language === "english") {
+    data = { message: `영어 토글 상태: ${toggleStatusEnglish}` };
+  }
+  if (language === "chinese") {
+    data = { message: `중국어 토글 상태: ${toggleStatusChinese}` };
+  }
+
+  // 실제 API 연동 추가해야함
+  try {
+    // const response = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // });
+    // const result = await response.json();
+    console.log(data);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+// DOM이 로드되면 실행
+document.addEventListener("DOMContentLoaded", function () {
   // 토글 ON/OFF 텍스트 색상 변경
   toggleList.forEach(($toggle) => {
     $toggle.addEventListener("click", () => {
@@ -22,6 +62,15 @@ document.addEventListener("DOMContentLoaded", function () {
       } else {
         statusOff.style.color = "#1b3133";
         statusOn.style.color = "#d4d4d4";
+      }
+
+      // 토글 상태 업데이트
+      if ($toggle.classList.contains("english")) {
+        toggleStatusEnglish = isActive; // 토글 상태 업데이트
+        saveLanguageSetting("english"); // 영어 토글 상태 저장 API 호출
+      } else if ($toggle.classList.contains("chinese")) {
+        toggleStatusChinese = isActive; // 토글 상태 업데이트
+        saveLanguageSetting("chinese"); // 중국어 토글 상태 저장 API 호출
       }
     });
   });
